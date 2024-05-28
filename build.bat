912207acde9af4c966088ed78415a4c5a443793f
Kat <kat@kattschan.co.uk>
2024-05-28 12:11:03 -0400

    asdf

---
+++ build.bat
@@ -0,0 +1,7 @@
+@ECHO OFF
+REM Zip up download.js, index.js, package.json, and .env into ../output/backend.zip
+mkdir ..\output
+mkdir ..\output\backend
+copy package.json ..\output\backend
+copy download.js ..\output\backend
+copy index.js ..\output\backend
